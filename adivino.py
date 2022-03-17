5540105b6a0b22755d6207bd0048fa76e7dc86be
Torr1co <fabriziotorrico27@gmail.com>
2022-03-17 09:28:18 -0300

    problema terminado

--- adivino.py
+++ adivino.py
@@ -3,9 +3,9 @@ import random
 # en la practica dice entre 0 y 100 asi que cambie el randrange hasta 101 y el print
 numero_aleatorio = random.randrange(101)
 gane = False
-print("Tenés 3 intentos para adivinar un entre 0 y 100")
-intento = 0
-while intento < 5 and not gane:
+print("Tenés 5 intentos para adivinar un entre 0 y 100")
+intento = 1
+while intento < 6 and not gane:
     numero_ingresado = int(input("Ingresa tu número: "))
     if numero_ingresado == numero_aleatorio:
         print("Ganaste! y necesitaste {} intentos!!!".format(intento))
